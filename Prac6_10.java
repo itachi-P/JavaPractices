ec6cbc8cf4cd87770eff08b9e9a1a69594cd0d29
unknown <itachip38@gmail.com>
2019-01-21 12:24:50 +0900

    modified Prac6_10.java

--- Prac6_10.java
+++ Prac6_10.java
@@ -4,7 +4,7 @@ package jouken;
 public class Prac6_10 {
 
 	public static void main(String[] args) {
-		int n1 = 11; int n2 = 20;
+		int n1 = 0; int n2 = 20;
 		System.out.println("フィボナッチ数列の" + n1 +"番目～" + n2 +"番目までの項");
 
 		for (int i = n1; i <= n2; i++) {
@@ -16,8 +16,7 @@ public class Prac6_10 {
 	}
 	//フィボナッチ数列のn番目の項を返す再帰的(自己参照)メソッド
 	private static int fibonacci(int num) {
-		//最初の2つの項は0番目0, 1番目1で基本的にnum>=2の前提
-		int a = 0; int b = 1;
+		//最初の2つの項は0番目0, 1番目1で基本的にnum>=2でロジックを考える前提
 		if (num == 0) {		//これが無いと延々自己参照し続けStackOverflowエラー発生
 			return 0;
 		} else if (num == 1) {
